9b7c7d3721ca437f738e78ae6c6dc7a25b322dad
Peter Hickling <developers@capacityapp.io>
2023-12-07 08:57:32 +0800

    Removed PUT that we don't need

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,12 +1,6 @@
 import got from 'got'
 import queryString from 'query-string'
 
-export const put = ({ endpoint, body, query }) =>
-  got.put(`${process.env.BASE_URL}/${endpoint}`, {
-    json: body,
-    headers: { Authorization: `Api-Key ${process.env.API_KEY}` },
-    search: queryString.stringify(query),
-  })
 export const post = ({ endpoint, body, query }) =>
   got.post(`${process.env.BASE_URL}/${endpoint}`, {
     json: body,
