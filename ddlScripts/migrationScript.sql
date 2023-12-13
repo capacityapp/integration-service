0ca0c17e1a2a5f8475617c9bc953296035967ac3
vishal melwani <vm4646@gmail.com>
2023-12-13 12:22:02 +0530

    add emloyee id

---
+++ ddlScripts/migrationScript.sql
@@ -0,0 +1,1 @@
+ALTER TABLE profiles ADD employeeId NVARCHAR(64) NULL;
--- ddlScripts/profiles.sql
+++ ddlScripts/profiles.sql
@@ -15,5 +15,6 @@ CREATE TABLE profiles
     chargeOutRateValue    INT,
 -- 3 letter currency code as per ISO 4217
     chargeOutRateCurrency NVARCHAR(3),
+    employeeId            NVARCHAR(64),
     PRIMARY KEY (email)
 );
